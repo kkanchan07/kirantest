dabab389c7b3101c0ae8af5a62a04e84ac42b3b9
Kiran <kiran.kanchan1@gmail.com>
2019-09-16 15:02:12 +0000

    adding new changes

--- big.c
+++ big.c
@@ -5,10 +5,9 @@ big()
     printf("\n Enter three different numbers: ");
     scanf("%lf %lf %lf", &n1, &n2, &n3);
     if( n1>=n2 && n1>=n3 )
-        printf("%.2f is the largest number.", n1);
+        printf("%.2f is the largest number. \n ", n1);
     if( n2>=n1 && n2>=n3 )
-        printf("%.2f is the largest number.", n2);
+        printf("%.2f is the largest number. \n ", n2);
     if( n3>=n1 && n3>=n2 )
-        printf("%.2f is the largest number.", n3);
-    //return 0;
+        printf("%.2f is the largest number. \n ", n3);
 }
--- makefile
+++ makefile
@@ -1,7 +1,9 @@
-Hub.exe : main.o  big.o fact.o palin.o
-	gcc -o Hub.exe main.o big.o fact.o palin.o
+Hub.exe : main.o  big.o fact.o palin.o rev.o
+	gcc -o Hub.exe main.o big.o fact.o rev.o palin.o
 main.o : main.c
 	gcc -c main.c
+rev.o : rev.c
+	gcc -c rev.c
 big.o : big.c
 	gcc -c big.c
 fact.o : fact.c
